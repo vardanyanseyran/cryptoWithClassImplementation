b62ef1a05175223ec0b73f26a1cbffc6e8f4004e
vardanyanseyran <116871843+vardanyanseyran@users.noreply.github.com>
2022-12-21 16:14:29 +0400

    Create ToxerSyuner.h

---
+++ ToxerSyuner.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Crypto.h"
+
+class Toxersyuner : public Crypto {
+	string sn;
+	int input;
+public:
+	ToxerSyuner(string sn, int input) : Crypto(str) {
+		sn = str;
+		input = k;
+	}
+
+	string dchange();
+	string nchange();
+};
