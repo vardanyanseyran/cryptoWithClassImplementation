e85861e3313d6571f9d275d5dfd5768eee832d5f
vardanyanseyran <116871843+vardanyanseyran@users.noreply.github.com>
2022-12-21 15:54:20 +0400

    Create Crypto.h

---
+++ Crypto.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class class_impl {
+	string s;
+public:
+	class_impl(string str) { s = str; }
+	string dchange() {};
+	string nchange() {};
+};
